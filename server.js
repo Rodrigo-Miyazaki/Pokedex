045fb0bf520ce09953608e6b377ca018e408706c
Rodrigo Miyazaki <rodrigo.mzk@gmail.com>
2021-03-22 00:24:42 -0300

    fix

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/pokedex-app-rmzk'));
+app.use(express.static('./dist/PokedexWeb'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/pokedex-app-rmzk/'}),
+    res.sendFile('index.html', {root: 'dist/PokedexWeb/'}),
 );
 
 // Start the app by listening on the default Heroku port
