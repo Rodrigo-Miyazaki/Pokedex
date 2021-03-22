1180cf2986fd4efe9253ea214cbf1856244a0338
Rodrigo Miyazaki <rodrigo.mzk@gmail.com>
2021-03-21 22:49:35 -0300

    PokedexAPI

---
+++ PokedexAPI/src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const routers = require("../routers/index.router");
+
+                                      
+app.use(cors());
+app.use(bodyParser.urlencoded({ "extended": "true" }));            
+app.use(bodyParser.json());                                     
+app.use(bodyParser.json({ type: "application/vnd.api+json" })); 
+
+app.use(routers);
+
+
+
+
+
+module.exports = app;
